213da614db434b5214176ca3172a1a01646f1a26
Dean Beebe <beebe.d@northeastern.edu>
2022-04-05 14:27:30 -0400

    finished admin & conductor model/schema/interface for mongoose

--- models/admin.ts
+++ models/admin.ts
@@ -1,1 +1,13 @@
+/**
+ * @file Implements model to represent administrators.
+ */
+import mongoose from "mongoose";
 
+export default interface Admin {
+    _id?: mongoose.Schema.Types.ObjectId,
+    username: string,
+    password: string,
+    name: string,
+    email: string,
+    yearsAtMBTA: string,
+}
