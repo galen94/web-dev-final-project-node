6650b0d3b6f32f50bea9f199561269311a4747ab
Galen Otten <galen.louise@gmail.com>
2022-04-26 13:47:55 -0400

    profile shows posts, posts page working

--- controllers/post-controller.ts
+++ controllers/post-controller.ts
@@ -11,11 +11,12 @@ export default class PostController implements PostControllerI {
         if (PostController.postController === null) {
             PostController.postController = new PostController();
 
-            app.post("/api/posts", PostController.postController.userPostsAPost);
+            app.post("/api/users/:uid/posts", PostController.postController.userPostsAPost);
             app.get("/api/posts/:pid", PostController.postController.findPostById);
             app.get("/api/posts", PostController.postController.findAllPosts);
+            app.get("/api/users/:uid/posts", PostController.postController.findAllPostsByUser);
             app.put("/api/posts/:pid", PostController.postController.userUpdatesAPost);
-            app.put("/api/posts/:pid", PostController.postController.updateStats);
+            app.put("/api/posts/:pid/stats", PostController.postController.updateStats);
             app.delete("/api/posts/:pid", PostController.postController.userDeletesAPost);
         }
 
@@ -26,7 +27,21 @@ export default class PostController implements PostControllerI {
 
 
     findAllPosts = (req: Request, res: Response) =>
-        PostController.postDao.findAllPosts().then((post: Post[]) => res.json(post));
+        PostController.postDao.findAllPosts().then((posts: Post[]) => res.json(posts));
+
+    findAllPostsByUser = (req: Request, res: Response) => {
+        console.log(req.params.uid)
+        // @ts-ignore
+        const userId = req.params.uid === "me" && req.session['profile'] ? req.session['profile']._id : req.params.uid;
+        console.log(userId)
+
+        if(userId === "me"){
+            res.sendStatus(503);
+            return;
+        }
+        PostController.postDao.findAllPostsByUser(userId).then((posts: Post[]) => res.json(posts));
+
+    }
 
     findPostById = (req: Request, res: Response) =>
         PostController.postDao.findPostById(req.params.pid).then((post:Post) => res.json(post));
@@ -38,8 +53,16 @@ export default class PostController implements PostControllerI {
     userDeletesAPost = (req: Request, res: Response) =>
         PostController.postDao.userDeletesAPost(req.params.pid).then(status => res.send(status));
 
-    userPostsAPost = (req: Request, res: Response) =>
-        PostController.postDao.userPostsAPost(req.body).then((post: Post) => res.json(post));
+    userPostsAPost = (req: Request, res: Response) => {
+        // @ts-ignore
+        const userId = req.params.uid === "me" && req.session['profile'] ? req.session['profile']._id : req.params.uid;
+
+        if(userId === "me"){
+            res.sendStatus(503);
+            return;
+        }
+        PostController.postDao.userPostsAPost(userId, req.body).then((post: Post) => res.json(post));
+    }
 
     userUpdatesAPost = (req: Request, res: Response) =>
         PostController.postDao.userUpdatesAPost(req.params.pid, req.body).then(status => res.send(status));
